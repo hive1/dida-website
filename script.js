85892cf18fee7ebf243a2044bfcc776e5b7cf2be
James Sears <93846353+hive1@users.noreply.github.com>
2025-05-01 14:38:34 -0400

    IM NOT UNDERSTANDING THIS SILLINESS MAN LIKE

---
+++ script.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: "body",
+  start: "top top",
+  end: "bottom bottom",
+  snap: {
+    snapTo: 1 / (document.querySelectorAll(".panel").length - 1), // snaps to each panel
+    duration: 0.5, // smooth snap duration (in seconds)
+    delay: 0.1,    // delay before snap occurs
+    ease: "power1.inOut" // easing function
+  },
+  markers: false
+});
