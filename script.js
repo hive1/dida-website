9c36186f0c7083c3ba83360f93b5bf9bb6569802
James Sears <93846353+hive1@users.noreply.github.com>
2025-05-01 15:42:25 -0400

    tryna implement scroll trigger, will report back later

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger); 
 
 ScrollTrigger.create({
   trigger: "body",
   start: "top top",
   end: "bottom bottom",
   snap: {
-    snapTo: 1 / (document.querySelectorAll(".panel").length - 1), // snaps to each panel
+    snapTo: 1 / (document.querySelectorAll(".section").length - 1), // snaps to each panel
     duration: 0.5, // smooth snap duration (in seconds)
     delay: 0.1,    // delay before snap occurs
     ease: "power1.inOut" // easing function
